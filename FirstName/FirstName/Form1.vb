580b9ac3f22a18e54cf759256f6668ef1a224253
Jana <wisn0101>
2013-09-16 15:24:43 -0500

    In class work Name

--- FirstName/FirstName/Form1.vb
+++ FirstName/FirstName/Form1.vb
@@ -3,6 +3,23 @@
 Public Class Form1
 
     Private Sub btnFirstName_Click(sender As System.Object, e As System.EventArgs) Handles btnFirstName.Click
+        Dim fullname As String = txtFullName.Text
+        'Read the input from the user
+        fullname = txtFullName.Text
 
+        'Make sure full name is trimmed, this is how
+        Dim fullnameTrimmed As String
+        fullnameTrimmed = fullname.Trim()
+
+        'Find the index of first space
+        Dim idxSpace As Integer
+        idxSpace = fullnameTrimmed.IndexOf(" ")
+
+        'Extract the first name
+        Dim firstName As String
+        firstName = fullnameTrimmed.Substring(0, idxSpace)
+
+        'Display tge results to the user
+        TextBox1.Text = firstName
     End Sub
 End Class
